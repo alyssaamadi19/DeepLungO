be8999c0a515da60a0fe394b307915c916924980
alyssaamadi19 <114242317+alyssaamadi19@users.noreply.github.com>
2022-12-02 12:37:55 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -29,7 +29,7 @@ st.markdown(
     """
 <style>
 .sidebar .sidebar-content {
-    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    background-image: linear-gradient(#2e7bcf);
     color: white;
 }
 </style>
