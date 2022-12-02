e0784f33d2ae14afedac597df6db095062528613
alyssaamadi19 <114242317+alyssaamadi19@users.noreply.github.com>
2022-12-02 12:56:05 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -6,24 +6,6 @@ import base64
 st.set_page_config(layout='wide', initial_sidebar_state='expanded', page_title = "DeepLungOp",)
 
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('doctor.png')  
-
-
-
 
 st.markdown(
     """
@@ -36,6 +18,13 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
 
 
 
