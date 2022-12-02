13130b3b983e58afa199b93708d43f982545f2c8
alyssaamadi19 <114242317+alyssaamadi19@users.noreply.github.com>
2022-12-02 02:31:00 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,19 +1,36 @@
 import streamlit as st
 import cv2
+import base64
 
-st.set_page_config(
-    page_title = "Multipage App",
-)
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+img = get_img_as_base64("doctor.png")
 
-page_bg = """
+page_bf_img = f"""
 <style>
 [data-testid = "stAppViewContainer"]{
 background-image: url("https://img.freepik.com/free-photo/abstract-orange-paint-background-acrylic-texture-with-marble-pattern_1258-90489.jpg?w=2000");
 background-size: cover;
 }
+
+[data-testid="stSidebar"] {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center;
+}}
 </style>
 """
-st.markdown(page_bg, unsafe_allow_html = True)
+
+
+
+st.set_page_config(
+    page_title = "Multipage App",
+)
+
+
+st.markdown(page_bf_img, unsafe_allow_html = True)
 
 st.title('Deep Learning Opacity Web Service')
 st.sidebar.success("Select a page above.")
