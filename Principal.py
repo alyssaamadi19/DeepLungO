9fcf41986021cfe275a68808fd0b416b84196419
alyssaamadi19 <114242317+alyssaamadi19@users.noreply.github.com>
2022-12-02 12:56:31 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -19,12 +19,6 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
 
 
 
